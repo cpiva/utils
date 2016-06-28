7f9ddff99fc24b7a7038f6542bb118370cc4b017
Carlo Piva <giancarlopiva@gmail.com>
2016-06-28 11:39:48 +1000

    minor

--- cm_api_scribble.py
+++ cm_api_scribble.py
@@ -5,31 +5,29 @@ from datetime import datetime, timedelta
 from cm_api.api_client import ApiResource
 from cm_api.endpoints.cms import ClouderaManager
 
-CM_HOST = "ec2-52-64-91-202.ap-southeast-2.compute.amazonaws.com"
+CM_HOST = "ec2-52-64-235-74.ap-southeast-2.compute.amazonaws.com"
  
-api = ApiResource(CM_HOST, username="admin", password="abc123")
+api = ApiResource(CM_HOST, username="admin", password="cr1pt0n1t3")
 cm = ClouderaManager(api)
 cluster = api.get_cluster('cluster')
 
-def list_hosts(self):
+def list_hosts():
   for host in api.get_all_hosts('full'):
     print host.hostname
 
-def list_roles(self):
+def list_roles():
+  hdfs = get_service("HDFS")
   for role in hdfs.get_all_roles():
     print role
 
-def list_services(self):
-  for service in cluster.get_all_services():
-    print service
-    if service.type == "HDFS":
-      hdfs = service
+def inspect_hdfs():
+  hdfs = get_service("HDFS")
   print hdfs.name, hdfs.serviceState, hdfs.healthSummary
   print hdfs.serviceUrl
   for chk in hdfs.healthChecks:
     print "%s --- %s" % (chk['name'], chk['summary'])
 
-def print_metrics(self):
+def print_metrics():
   from_time = datetime.fromtimestamp(time.time() - 1800*2*24) # 24 hrs.
   to_time = datetime.fromtimestamp(time.time())
   query = "select files_total, dfs_capacity, dfs_capacity_used " \
@@ -42,23 +40,28 @@ def print_metrics(self):
     for point in ts.data:
       print "%s:\t%s" % (point.timestamp.isoformat(), point.value)
 
-def print_yarn_apps(self):
+def print_yarn_apps():
   now = datetime.today()
-  lastHour = datetime.today() - timedelta(hours = 1)
-  for service in cluster.get_all_services():
-    print service
-    if service.type == "YARN":
-      yarn = service
-  for app in yarn.get_yarn_applications(lastHour, now, filter_str = "state=RUNNING").applications:
+  lastHr = datetime.today() - timedelta(hours = 1)   
+  for app in get_service("YARN").get_yarn_applications(lastHr, now, filter_str = "state=RUNNING").applications:
     print app
 
-def add_host(self):
+def add_new_host():
   name = 'ip-172-31-1-41.ap-southeast-2.compute.internal'
   host = api.create_host(name, name, "172.31.1.41", "/default")
   xid = str(uuid.uuid4()).replace('-','')
   hdfs.create_role("hdfs-DATANODE-" + xid, "DATANODE", name)
 
-def inspect_hosts(self):
+def delete_host():
+  api.delete_host()
+
+def add_datanode_role():
+  name = 'ip-172-31-9-17.ap-southeast-2.compute.internal'
+  xid = str(uuid.uuid4()).replace('-','')
+  #get_service("HDFS").create_role("hdfs-DATANODE-" + xid, "DATANODE", name)
+  get_service("HDFS").create_role("hdfs-DATANODE-ac05b2e3cafc01531b61c12e304b2c04", "DATANODE", name)
+
+def inspect_hosts():
   print "Inspecting hosts. This might take a few minutes."
   cmd = cm.inspect_hosts()
   while cmd.success == None:
@@ -68,8 +71,25 @@ def inspect_hosts(self):
       exit(0)
   print "Hosts successfully inspected: \n" + cmd.resultMessage
 
-def restart_hdfs(self):
-  cmd = hdfs.restart()
+def get_service(type):
+  for service in cluster.get_all_services():
+    if service.type == type:
+      return service
+
+def restart_hdfs():
+  cmd = get_service("HDFS").restart()
   print cmd.active
   cmd = cmd.wait()
   print "Active: %s. Success: %s" % (cmd.active, cmd.success)
+
+def main():
+  #restart_hdfs()  
+  #inspect_hosts()
+  #print_yarn_apps()
+  #inspect_hdfs()
+  #list_roles()
+  list_hosts()
+  #add_datanode_role()
+  delete_host()
+  
+if __name__ == "__main__": main()
